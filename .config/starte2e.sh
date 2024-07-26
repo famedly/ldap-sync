11ee72d4de2116813ad93df4f21dcd3f9306c532
Tristan Daniël Maat <t.maat@famedly.com>
2024-07-26 16:59:06 +0200

    ci: Fix missing entry to `PATH`

--- .config/starte2e.sh
+++ .config/starte2e.sh
@@ -2,6 +2,10 @@
 
 set -eu
 
+# CI does not add /usr/bin to $PATH for some reason, which means we
+# lack docker
+export PATH="${PATH}:/usr/bin"
+
 # Shut down any still running test-setup first
 docker compose --project-directory ./tests/environment down -v test-setup || true
 docker compose --project-directory ./tests/environment up --wait
