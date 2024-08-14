2cf635dd1c2bfb4face1b888d9f3e1e866cfec97
Tristan Daniël Maat <t.maat@famedly.com>
2024-08-14 12:23:57 +0200

    ci: Add code coverage to the test workflow

--- .config/starte2e.sh
+++ .config/starte2e.sh
@@ -6,6 +6,10 @@ set -eu
 # lack docker
 export PATH="${PATH}:/usr/bin"
 
+# Make sure the containers can write some files that need to be shared
+touch tests/environment/zitadel/service-user.json
+chmod a+rw tests/environment/zitadel/service-user.json
+
 # Shut down any still running test-setup first
 docker compose --project-directory ./tests/environment down -v test-setup || true
 docker compose --project-directory ./tests/environment up --wait
