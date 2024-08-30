49bb8dc4824453383a5a3b528a7b01d60bebe384
Matheus Zaniolo <m.zaniolo@famedly.com>
2024-08-30 11:37:29 +0200

    fix: Reissue of test certificates
    Reissue of expired test certificates. The new expiration date is Aug 2034

--- tests/environment/certs/generate-certs.sh
+++ tests/environment/certs/generate-certs.sh
@@ -12,7 +12,7 @@ openssl req -x509 -new -nodes -sha256 -newkey rsa:2048 \
 chmod go+r server.key
 
 openssl x509 -outform pem -in server.crt -out ca.crt
-openssl req -x509 -nodes -sha256 -newkey rsa:2048 \
+openssl req -x509 -nodes -days 3650 -sha256 -newkey rsa:2048 \
 		-CAkey server.key \
 		-CA ca.crt \
 		-keyout client.key \
