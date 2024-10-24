e0850652ba28445fd89199cf4b34fae9b5062a12
Matheus Zaniolo <m.zaniolo@famedly.com>
2024-10-24 12:31:26 +0200

    test: Fix certificate being not trusted on MacOS

--- tests/environment/certs/generate-certs.sh
+++ tests/environment/certs/generate-certs.sh
@@ -1,20 +1,25 @@
 #!/bin/sh
 set -eux
 
+script_dir=$(dirname $0)
+
+# We need to set EKUs (extendedKeyUsage) otherwise MacOS won't trust
+# the certificate
 openssl req -x509 -new -nodes -sha256 -newkey rsa:2048 \
-		-keyout server.key \
-		-out server.crt \
+		-keyout $script_dir/server.key \
+		-out $script_dir/server.crt \
 		-subj "/C=DE/CN=example.org" \
-		-addext "subjectAltName = DNS:zitadel, DNS:localhost"
+		-addext "subjectAltName = DNS:zitadel, DNS:localhost" \
+		-addext "extendedKeyUsage = serverAuth, clientAuth"
 
 # These keys are not actually secret, and when passed into the docker
 # container the server key needs to be readable by the container user
-chmod go+r server.key
+chmod go+r $script_dir/server.key
 
-openssl x509 -outform pem -in server.crt -out ca.crt
+openssl x509 -outform pem -in $script_dir/server.crt -out $script_dir/ca.crt
 openssl req -x509 -nodes -days 3650 -sha256 -newkey rsa:2048 \
-		-CAkey server.key \
-		-CA ca.crt \
-		-keyout client.key \
-		-out client.crt \
+		-CAkey $script_dir/server.key \
+		-CA $script_dir/ca.crt \
+		-keyout $script_dir/client.key \
+		-out $script_dir/client.crt \
 		-subj "/CN=admin.example.org"
